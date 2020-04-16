9c290bfe506357dffad75ba013a2e16ebf5d0632
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-16 20:19:27 +0000

    Deploy IndiaSmile/IndiaSmile.github.io to github.com/IndiaSmile/IndiaSmile.github.io.git:master

---
+++ _nuxt/1c1b1a8dfdd02b764e4a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{625:function(e,n,t){"use strict";t.r(n);var o={title:"Redirecting to COVID page",middleware:function(e){return(0,e.redirect)("/covid")}},d=t(6),component=Object(d.a)(o,void 0,void 0,!1,null,null,null);n.default=component.exports}}]);
